40610dc1aab6661bd3853a9f089199b5414681d2
caioaugustoo <caioamfr@gmail.com>
2023-06-10 21:52:14 -0300

    feat: create helper functions to get dynamic select labels

---
+++ utils/get-role-select-option-label.ts
@@ -0,0 +1,13 @@
+import { DEFAULT_OPTION_VALUES } from "@/constants"
+import { GuildRole } from "@/hooks"
+
+export const getRoleSelectOptionLabel = (
+  value: string,
+  roles: GuildRole[] | undefined
+): string => {
+  if (value === DEFAULT_OPTION_VALUES.allowedRole.id) {
+    return "All Roles"
+  }
+
+  return roles?.find((role) => role.id === value)?.name || "All Roles"
+}
