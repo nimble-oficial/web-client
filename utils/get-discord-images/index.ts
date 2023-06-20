89b20c773f780438ea3e2feccadc79c1034a4de5
caioaugustoo <caioamfr@gmail.com>
2023-06-19 23:23:46 -0300

    tests: add few unit tests

--- utils/get-discord-images/index.ts
+++ utils/get-discord-images/index.ts
@@ -1,3 +1,7 @@
 export const getGuildIcon = (guildId: string, guildIcon: string): string => {
+  if (!guildId || !guildIcon) {
+    return ""
+  }
+
   return `https://cdn.discordapp.com/icons/${guildId}/${guildIcon}.png`
 }
